2353105f67646acab4717ff381cc2dbe027561c1
Gabriel Royce <geroyceus@gmail.com>
2023-01-30 12:17:59 -0500

    Converted into a baseline format for the website -Removed unecessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
 import '../styling/homePage.scss';
 function Index(props) {
@@ -7,18 +6,6 @@ function Index(props) {
 			<div className="homeText">
 				<h1>Your new best friend</h1>
 				<h2>Welcome to the newest event planner catered to artists</h2>
-			</div>
-			<div className="buttonDiv">
-				<Link to={`/`} style={{ textDecoration: 'none' }} disabled>
-					<button className="loginButton">
-						<span>Login</span>
-					</button>
-				</Link>
-				<Link to={`/`} style={{ textDecoration: 'none' }} disabled>
-					<button className="registerButton" disabled>
-						<span>Register</span>
-					</button>
-				</Link>
 			</div>
 		</div>
 	);
