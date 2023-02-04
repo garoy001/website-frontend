1708a5f4044c8ebf35776203efa8628101a86737
Gabriel Royce <geroyceus@gmail.com>
2023-02-04 12:39:26 -0800

    added avatar, further styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import '../styling/homePage.scss';
 import "../styling/projects.scss"
 import Projects from '../components/projects';
+import avatar from '../assets/avatar.png'
 function Index(props) {
 	return (
 		<div>
 <div className="homeBody fade-in">
+			<div className="introBox">
+			<img src={avatar} />
 			<div className="homeText">
+				
 				<h1>Hi! I'm Gabe</h1>
 				<h2>
 					I'm a full stack web developer with a focus on always improving.
 				</h2>
 			</div>
+			</div>
+			
 			<div className="arrow bounce">
 				<p className="fa fa-arrow-down fa-2x" href="#"></p>
 			</div>
-			<div className='dividingLine'></div>
 			
 		</div>
 		<div className="projects">
